7704a51afeec31429779d7fd0eae7d4be36d83c6
Frank Jensen <me@taheilo.com>
2012-10-24 16:28:54 +0200

    Added a proper 404 page
    - Updated the 404 page
    - Removed some of the old js includes (from h5b)

--- app/views/error/error_404.html.php
+++ app/views/error/error_404.html.php
@@ -1,1 +1,9 @@
-Sorry the page you were looking for was not found
+<div class="row">
+  <div class="span12">
+    <div class="hero-unit">
+      <h1>/404</h1>
+      <p>Sorry, the page <code><?= $_SERVER['REQUEST_URI'] ?></code> could not be found.</p>
+      <a href="https://github.com/taheilo/blackbird" class="btn btn-primary btn-large">View Blackbird on Github</a>
+    </div>
+  </div>
+</div>
--- app/views/layouts/application.html.php
+++ app/views/layouts/application.html.php
@@ -14,7 +14,6 @@
 
       <link rel="stylesheet" href="/assets/css/bootstrap/bootstrap.min.css">
       <link rel="stylesheet" href="/assets/css/bootstrap/bootstrap-responsive.min.css">
-      <script src="js/vendor/modernizr-2.6.1.min.js"></script>
   </head>
   <body>
     <!--[if lt IE 7]>
@@ -33,9 +32,8 @@
     </div>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+    <script>window.jQuery || document.write('<script src="/assets/js/jquery-1.8.2.min.js"><\/script>')</script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
